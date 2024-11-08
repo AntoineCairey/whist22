5024a2e6736c26229f460a88dbc839e642f56b82
Antoine Cairey <cairey.antoine@gmail.com>
2024-11-08 18:35:21 +0100

    better rules page

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -6,41 +6,72 @@ export default function Rules() {
   return (
     <div className="rules">
       <button onClick={() => navigate("/")}>⬅️ Retour</button>
-      <br />
-      <br />
 
-      <h3>Règles du tarot africain</h3>
-      <div>
-        <p>
-          Au tarot africain, vous jouez contre des adversaires (3 ici). Vous
-          commencez la partie avec 3 vies chacun. Si vous tombez à 0 vies, vous
-          êtes éliminé. Votre but est d'être le dernier joueur en vie.
-        </p>
-        <p>
-          Le jeu se joue avec les atouts d'un jeu de tarot français (1 à 21) et
-          l'excuse. La partie se joue en manches successives. A chaque début de
-          manche, chaque joueur doit estimer le nombre de plis qu'il va
-          réaliser. On joue ensuite les différents plis. A la fin de la manche,
-          on compare les plis réalisés par un joueur au nombre annoncé, et on
-          retire la différence à son nombre de points de vies. S'il tombe à 0
-          vies, il est éliminé.
-        </p>
-        <p>
-          La 1e manche se joue avec 5 cartes par personne, puis la 2e avec 4
-          cartes, puis la 3e à 3 cartes, etc. La manche à 1 carte est
-          particulière : les joueurs ne voient pas leur propre carte, mais
-          voient celle des autres joueurs. La manche suivante se fait avec 5
-          cartes par personne, puis 4, et ainsi de suite jusqu'à ce qu'il y ait
-          un vainqueur.
-        </p>
-        <p>
-          Dans un pli, chaque joueur joue une carte à tour de rôle, et la plus
-          grande valeur remporte le pli. Il n'y a pas d'obligation de "monter",
-          on peut jouer la carte de son choix. L'excuse est une carte
-          particulière : au moment où il la pose, le joueur décide si elle vaut
-          0 ou 22.
-        </p>
-      </div>
+      <h2>Règles du tarot africain</h2>
+
+      <br />
+      <h3>But du jeu</h3>
+      <p>
+        Le tarot africain est un jeu de cartes qui se joue avec les atouts d'un
+        jeu de tarot français (1 à 21) et l'excuse. Vous jouez contre des
+        adversaires (3 ici). Vous commencez la partie avec 3 vies chacun. Si
+        vous tombez à 0 vies, vous êtes éliminé. Votre but est d'être le dernier
+        joueur en vie.
+      </p>
+
+      <br />
+      <h3>Déroulement d'un partie</h3>
+      <p>
+        La partie se joue en manches successives. On commence par distribuer les
+        cartes (5 cartes par personne pour la 1e manche). Puis on passe à la
+        phase d'annonces.
+      </p>
+      <p>
+        Lors des annonces, chaque joueur à son tour doit annoncer le nombre de
+        plis qu'il pense remporter pendant cette manche. On passe ensuite à la
+        phase de jeu, en commençant le 1er pli.
+      </p>
+      <p>
+        Lors d'un pli, chaque joueur joue à son tour une carte de sa main. La
+        carte la plus grande remporte le pli. Le gagnant du pli commence le pli
+        suivant. On continue jusqu'à ne plus avoir de cartes en main : c'est la
+        fin de la manche.
+      </p>
+      <p>
+        On va ensuite comparer, pour chaque joueur, le nombre de plis réalisés à
+        l'annonce faite en début de manche. S'il n'a pas fait le bon nombre de
+        plis, on retire la différence de son total de points de vie. Si un
+        joueur arrive à 0 vies, il est éliminé.
+      </p>
+      <p>
+        Une fois les vies retirées, s'il y a encore au moins 2 joueurs en vie,
+        on relance une nouvelle manche avec 1 carte de moins en main, et ainsi
+        de suite. Le jeu s'arrête lorsque qu'il ne reste qu'un seul joueur en
+        vie, il est déclaré vainqueur.
+      </p>
+
+      <br />
+      <h3>Précisions</h3>
+      <p>
+        La première manche se joue avec 5 cartes par personne. La 2e manche avec
+        4 cartes, la 3e à 3 cartes, etc. La manche à 1 carte est particulière :
+        les joueurs ne voient pas leur propre carte, mais voient celle des
+        autres joueurs. La manche suivante se fait avec 5 cartes par personne,
+        puis 4, et ainsi de suite jusqu'à ce qu'il y ait un vainqueur.
+      </p>
+      <p>
+        Lors des annonces, le donneur, qui est le dernier à parler, doit
+        respecter une contrainte pour son annonce : la somme des annonces de la
+        manche ne doit pas être égale au nombre de cartes en main.
+      </p>
+      <p>
+        Lors d'un pli, il n'y a pas d'obligation de "monter", vous pouvez jouer
+        n'importe quelle carte que vous avez en main.
+      </p>
+      <p>
+        L'excuse est une carte "joker" très puissante. Lorsque vous la jouez,
+        vous choisissez si elle vaut 0 ou 22.
+      </p>
     </div>
   );
 }
